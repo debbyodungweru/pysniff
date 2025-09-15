6169042004cc18c7f46247c8c33dd5138964e7ef
Deborah Odungweru <deborah.odungweru@gmail.com>
2025-09-15 16:25:03 +0100

    update: minor formatting

--- pysniff/rules/rule_sql_injection.py
+++ pysniff/rules/rule_sql_injection.py
@@ -10,10 +10,13 @@ class RuleSQLInjection(BaseRule):
     id = "PS003"
     name = "sql_injection"
     message = "Possible SQL injection via {}"
-    full_description = "Building SQL queries via string concatenation or f-strings may allow SQL injection. Use parameterized queries instead."
+    full_description = ("Building SQL queries via string concatenation "
+                        "or f-strings may allow SQL injection. "
+                        "Use parameterized queries instead.")
     help_uri = "https://cwe.mitre.org/data/definitions/89.html"
     cwe = pysniff.CWE("89",
-                      "Improper Neutralization of Special Elements used in an SQL Command ('SQL Injection')")
+                      "Improper Neutralization of Special Elements used in an "
+                      "SQL Command ('SQL Injection')")
 
     def check(self, node, context):
 
@@ -21,16 +24,23 @@ class RuleSQLInjection(BaseRule):
         if isinstance(node.func, ast.Attribute) and node.func.attr == 'execute':
 
             # ensure the method is called on a cursor object
-            if isinstance(node.func.value, ast.Name) and node.func.value.id in self._get_sql_cursor_vars(context.get("root_node")):
+            if (isinstance(node.func.value, ast.Name) and
+                node.func.value.id in self._get_sql_cursor_vars(context.get("root_node"))):
 
                 if node.args:
                     execute_arg = node.args[0]
                     source = None
-                    if isinstance(execute_arg, ast.BinOp) and isinstance(execute_arg.op, ast.Add):
+                    # check for string concatenation
+                    if (isinstance(execute_arg, ast.BinOp) and
+                            isinstance(execute_arg.op, ast.Add)):
                         source = "string concatenation"
+                    # check for use of f-strings
                     elif isinstance(execute_arg, ast.JoinedStr):
                         source = "f-string"
-                    elif isinstance(execute_arg, ast.Call) and isinstance(execute_arg.func, ast.Attribute) and execute_arg.func.attr == "format":
+                    # check for use of str.format()
+                    elif (isinstance(execute_arg, ast.Call) and
+                          isinstance(execute_arg.func, ast.Attribute) and
+                          execute_arg.func.attr == "format"):
                         source = ".format()"
 
                     if source is not None:
